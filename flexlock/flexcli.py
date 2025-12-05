fbea958b1f2b4ddffaf0e7c1478bdd9f6a7a0b93
Quentin Febvre <quentin.febvre@imt-atlantique.fr>
2025-12-05 12:56:55 +0000

    Allow multi taskfile

--- flexlock/flexcli.py
+++ flexlock/flexcli.py
@@ -68,6 +68,7 @@ def flexcli(default_config=None, description=None, debug=None):
             parser.add_argument(
                 "--tasks",
                 default=None,
+                nargs="*",
                 help="Path to a file with a list of tasks (e.g., .txt, .yaml).",
             )
             parser.add_argument(
@@ -183,7 +184,9 @@ def flexcli(default_config=None, description=None, debug=None):
                 return main_fn(cfg)
 
             # 2. Load tasks for batch execution
-            tasks = load_tasks(cli_args.tasks, cli_args.tasks_key, cfg)
+            tasks = []
+            for t in cli_args.tasks:
+                tasks.extend(load_tasks(t, cli_args.tasks_key, cfg))
             logger.info(f"{len(tasks)} tasks loaded")
 
             if not tasks:
